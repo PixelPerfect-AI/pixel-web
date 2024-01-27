56f58335f8d18253a19d8d541d99706831224a4e
Fathaah <abdulfta12@gmail.com>
2024-01-27 17:19:27 +0100

    UI touchups and we will be back soon message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,12 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1 h-full mx-auto flex justify-center items-center">PixelPerfect</h1>
-		<h2 class="h2">Where your content creation dreams come alive!</h2>
+<div class="flex justify-center items-center h-screen">
+	<div class="containerh-full mx-auto flex justify-center items-center">
+		<!-- Add flex-col class -->
+		<div class="space-y-5">
+			<h1 class="h1 h-full mx-auto flex justify-center items-center">PixelPerfect AI</h1>
+			<h2 class="h2">Where your content creation dreams come alive!</h2>
+			<p class="h-full mx-auto flex justify-center items-center">🚧🔨 This page is a work in progress and we will be back with more soon!</p>
+		</div>
 	</div>
 </div>
+
