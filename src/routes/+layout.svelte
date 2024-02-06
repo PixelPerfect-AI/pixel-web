93a48e09adaa14c1c9716eacb577c21ba620f7f2
Fathaah <abdulfta12@gmail.com>
2024-02-06 21:08:55 +0100

    fix import

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,9 +4,6 @@
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
-
-	// Logo
-	import Logo from '$lib/icons/logo.svelte';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 </script>
 
